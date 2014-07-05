0f6508110af9c74f13a92488ef4cc7ce13328de8
Viresh <viresh.ramdatmisier@outlook.com>
2014-07-05 12:24:37 -0700

    updates

--- BTCAddress.py
+++ BTCAddress.py
@@ -13,12 +13,17 @@ class BTCAddress:
 
 	def GenerateAddress(self, data):
 		e_data = data.encode(self.__encode_type)
-		hash = haslib.sha256(e_data)
-		hex_digest = hash.hex_digest()
-		print(hex_digest)
+		hash = haslib.sha256(e_data).hexdigest()
+
+		print(hash)
+
+		vhash = haslib.new('ripemd160')
+		vhash.update(hash)
+
+		print(vhash.hexdigest())
+
 		#https://en.bitcoin.it/wiki/Technical_background_of_Bitcoin_addresses#How_to_create_Bitcoin_Address
 		#hash data using sha256
-		#hash the hash with ripemd160
 		#verion byte (0x00) is added to the beginning (CONCAT)
 		#checksum - first 4 bytes of sha256(sha256(vhash)) is added to the end
 		#convert to base58
