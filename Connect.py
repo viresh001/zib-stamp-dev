a85ea30ad205af0519a29c064439b3a1c0dac3d8
Viresh <viresh.ramdatmisier@outlook.com>
2014-07-05 11:42:20 -0700

    updates

--- Connect.py
+++ Connect.py
@@ -17,6 +17,15 @@ class Connect:
 		self.__api_key = api_key
 		self.__api_secret = api_secret
 
+	def GenerateAddress(self, data):
+		#https://en.bitcoin.it/wiki/Technical_background_of_Bitcoin_addresses#How_to_create_Bitcoin_Address
+		#hash data using sha256
+		#hash the hash with ripemd160
+		#verion byte (0x00) is added to the beginning (CONCAT)
+		#checksum - first 4 bytes of sha256(sha256(vhash)) is added to the end
+		#convert to base58
+
+
 	def __nonce(self):
 		self.__nonce_v = '{:.10f}'.format(time.time() * 1000).split('.')[0]
 		print("nonce: " + self.__nonce_v)
