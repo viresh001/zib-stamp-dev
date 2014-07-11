4c3472c041fccab230c52b921849ccb41140f086
Viresh <viresh.ramdatmisier@outlook.com>
2014-07-11 16:11:39 -0700

    updates

--- BTCAddress.py
+++ BTCAddress.py
@@ -6,29 +6,30 @@ import hmac
 import hashlib
 import ecdsa
 
+
 class BTCAddress:
-	__nonce_v = ''
-	__encode_type = 'utf-8'
+    __nonce_v = ''
+    __encode_type = 'utf-8'
 
-	def __init__(self):
-		print ("constructor")
-
-
-	def __nonce(self):
-		self.__nonce_v = '{:.10f}'.format(time.time() * 1000).split('.')[0]
-		print("nonce: " + self.__nonce_v)
-
-	def __get_timestamp(self):
-		print("__get_timestamp")
+    def __init__(self):
+        print("constructor")
 
 
-	def generate_address(self, data):
-		e_data = data.encode(self.__encode_type)
-		hash = hashlib.sha256(e_data).hexdigest()
+    def __nonce(self):
+        self.__nonce_v = '{:.10f}'.format(time.time() * 1000).split('.')[0]
+        print("nonce: " + self.__nonce_v)
 
-		print(hash)
+    def __get_timestamp(self):
+        print("__get_timestamp")
 
-		#vhash = hashlib.new('ripemd160')
-		#vhash.update(hash)
 
-		#print(vhash.hexdigest())
+    def generate_address(self, data):
+        e_data = data.encode(self.__encode_type)
+        hash = hashlib.sha256(e_data).hexdigest()
+
+        print(hash)
+
+        # vhash = hashlib.new('ripemd160')
+        # vhash.update(hash)
+
+        #print(vhash.hexdigest())
--- Main.py
+++ Main.py
@@ -2,9 +2,12 @@ import sys
 import BTCAddress
 
 def main():
-	print("main")
-	addy = BTCAddress.BTCAddress()
-	addy.generate_address("hello")
+    print("main")
+    addy = BTCAddress.BTCAddress()
+    addy.generate_address("hello")
+
+    addy2 = BTCAddress.BTCAddress()
+    addy2.generate_address("world")
 
 if __name__ == "__main__":
 	main()
