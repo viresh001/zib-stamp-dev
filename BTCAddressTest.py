54a086754fd23093f33dbcc8ebcdf45bdc5fe849
Viresh <viresh.ramdatmisier@outlook.com>
2014-07-13 20:40:15 -0700

    updates

--- BTCAddressTest.py
+++ BTCAddressTest.py
@@ -2,12 +2,13 @@ import unittest
 import BTCAddress
 
 class BTCAddressTest(unittest.TestCase):
+
     def test_get_btc_address(self):
         test_addy = BTCAddress.BTCAddress()
         runs=0
-        while runs < 100:
-            btc_address001 = test_addy.get_btc_address("viresh")
-            btc_address002 = test_addy.get_btc_address2("viresh")
+        while runs < 1000000:
+            btc_address001 = test_addy.get_btc_address("war-pig")
+            btc_address002 = test_addy.get_btc_address2("war-pig")
             self.assertEqual(btc_address001, btc_address002)
             runs += 1
 
