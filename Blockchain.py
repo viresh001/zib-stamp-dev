313e5ff71c5b9e154b81b405e359950c134655b0
Viresh <viresh.ramdatmisier@outlook.com>
2014-07-20 21:15:06 -0700

    updates

--- Blockchain.py
+++ Blockchain.py
@@ -2,6 +2,7 @@ import urllib.request
 
 class Blockchain:
 
+    __send_btc_address = '1HRz4Jb9rthPDUz4mamMktn5fkwLGwcZjj'
     __blockchain_url = 'http://blockchain.info/q/'
     __verbs = {0: 'addressfirstseen/',
                1: 'addressbalance/'}
